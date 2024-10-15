852683ee7e15cfa895e1114b69473adddce35c97
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-15 10:12:24 +0200

    create LunchTrayScreen enum class

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -23,7 +24,13 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.ui.OrderViewModel
 
-// TODO: Screen enum
+enum class LunchTrayScreen(@StringRes val title: Int) {
+    Start(title = R.string.start_order),
+    Entree(title = R.string.choose_entree),
+    DishMenu(title = R.string.choose_side_dish),
+    Accompaniment(title = R.string.choose_accompaniment),
+    CheckOut(title = R.string.order_checkout)
+}
 
 // TODO: AppBar
 
