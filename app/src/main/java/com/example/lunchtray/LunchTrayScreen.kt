5f7b8f6bccd2062152a56aed4312205571ac858b
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-15 10:45:51 +0200

    implement navigation

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -16,20 +16,32 @@
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.ui.OrderViewModel
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.model.OrderUiState
+import com.example.lunchtray.ui.AccompanimentMenuPreview
+import com.example.lunchtray.ui.AccompanimentMenuScreen
+import com.example.lunchtray.ui.CheckoutScreen
+import com.example.lunchtray.ui.EntreeMenuScreen
+import com.example.lunchtray.ui.SideDishMenuScreen
+import com.example.lunchtray.ui.StartOrderScreen
 
 enum class LunchTrayScreen(@StringRes val title: Int) {
     Start(title = R.string.start_order),
@@ -70,6 +82,58 @@ fun LunchTrayApp(
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
-        // TODO: Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = LunchTrayScreen.Start.name) {
+                StartOrderScreen(onStartOrderButtonClicked = {
+                    navController.navigate(LunchTrayScreen.Entree.name)
+                })
+            }
+
+            composable(route = LunchTrayScreen.Entree.name) {
+                EntreeMenuScreen(
+                    options = DataSource.entreeMenuItems,
+                    onCancelButtonClicked = { /*TODO*/ },
+                    onNextButtonClicked = {
+                        navController.navigate(LunchTrayScreen.DishMenu.name)
+                    },
+                    onSelectionChanged = {}
+                )
+            }
+
+            composable(route = LunchTrayScreen.DishMenu.name) {
+                SideDishMenuScreen(
+                    options = DataSource.sideDishMenuItems,
+                    onCancelButtonClicked = { /*TODO*/ },
+                    onNextButtonClicked = { 
+                        navController.navigate(LunchTrayScreen.CheckOut.name)
+                    },
+                    onSelectionChanged = {}
+                )
+            }
+
+            composable(route = LunchTrayScreen.Accompaniment.name) {
+                AccompanimentMenuScreen(
+                    options = DataSource.accompanimentMenuItems,
+                    onCancelButtonClicked = { /*TODO*/ },
+                    onNextButtonClicked = { 
+                        navController.navigate(LunchTrayScreen.Accompaniment.name)
+                    },
+                    onSelectionChanged = {}
+                )
+            }
+
+            composable(route = LunchTrayScreen.CheckOut.name) {
+                CheckoutScreen(
+                    orderUiState = OrderUiState(),
+                    onNextButtonClicked = {
+                        navController.navigate(LunchTrayScreen.CheckOut.name)
+                    },
+                    onCancelButtonClicked = { /*TODO*/ })
+            }
+        }
     }
 }
